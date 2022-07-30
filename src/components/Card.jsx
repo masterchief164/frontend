976c7b2240bbe4a788e93a1bb890cbda751abcf9
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-07-30 22:07:10 +0530

    ready for deployment

---
+++ src/components/Card.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import '../stylesheets/Card.css';
+import IMG from '../assets/Id.jpg';
+
+
+const Card = ({
+  item,
+  setPopupTrigger,
+  setPopupData,
+}) => {
+  const image = item.image === undefined || item.image === 'default' || item.image === '' ? IMG : item.image;
+  let date = new Date(item.dateTime).toDateString()
+    .substring(4);
+  date = date === 'lid Date' ? '22 Jan,2022' : date;
+  const title = item.title === undefined ? 'default' : item.title;
+  const description = item.description === undefined ? 'default' : item.description;
+  // const location = item.location === undefined ? 'default' : item.location;
+  const button = (item.type) === 'Lost' ? 'Found' : 'Claim';
+
+  const clickHandler = async () => {
+    setPopupData(item);
+    setPopupTrigger(true);
+  };
+
+  return (
+    <>
+      <div className="card-box">
+        <div className="popup-header">
+          <div className="Image-box">
+            <img src={image} alt="img"></img>
+          </div>
+          <div className="Title-box">
+            <div className="Main-title">
+              <p>{title}</p>
+            </div>
+            <div className="Description">
+              {item.claimedBy ? <p className="tags" style={{ color: 'green' }}>Status : {button === 'Found' ? button : button + 'ED'} </p> :
+                <p className="tags" style={{ color: 'red' }}>Status : Not yet {button === 'Found' ? button : button + 'ED'} </p>}
+              <h3>Description</h3>
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
+        <div className="popup-footer">
+          <p>Reported on<br /><strong>{date}</strong></p>
+          <button onClick={clickHandler}>{button} it</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
---
+++ src/routes/homepage.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import '../stylesheets/Homepage.css';
+import HomepageListItems from '../components/homepageItemsList';
+import banner from '../assets/banner.png';
+import { UserContext } from '../utils/UserContext';
+
+function Homepage() {
+  const [, , , setPageNumber] = React.useContext(UserContext);
+
+  useEffect(() => {
+    setPageNumber(0);
+  }, []);
+
+  return (
+    <>
+      <section className="BannerBox">
+        <div className="banner">
+          <h1>Lost <span> & </span> Found</h1>
+          <h3>We help everyone to get their lost things !</h3>
+          <img src={banner} alt="banner" />
+        </div>
+        <div className="itemTags">
+          <div className="tag">
+            <h3>LOST ITEMS : 30</h3>
+          </div>
+          <div className="tag">
+            <h3>FOUND ITEMS : 30</h3>
+          </div>
+        </div>
+      </section>
+
+      <section className="ListBox">
+        <div className="listContainer">
+          <HomepageListItems />
+          <HomepageListItems />
+          <HomepageListItems />
+          <HomepageListItems />
+          <HomepageListItems />
+          <HomepageListItems />
+        </div>
+        <div className="listContainer">
+          <HomepageListItems />
+          <HomepageListItems />
+          <HomepageListItems />
+          <HomepageListItems />
+          <HomepageListItems />
+          <HomepageListItems />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Homepage;
